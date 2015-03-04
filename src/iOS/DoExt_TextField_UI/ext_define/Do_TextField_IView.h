0f9675922cad0312ac7673eb24e01c50668a2aed
OstrichRun <347647092@qq.com>
2015-03-04 15:30:58 +0800

    iOS
    拆分

---
+++ src/iOS/DoExt_TextField_UI/ext_define/Do_TextField_IView.h
@@ -0,0 +1,24 @@
+//
+//  TYPEID_UI.h
+//  DoExt_UI
+//
+//  Created by @userName on @time.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol Do_TextField_IView <NSObject>
+
+@required
+//属性方法
+- (void)change_text:(NSString *)newValue;
+- (void)change_fontColor:(NSString *)newValue;
+- (void)change_fontSize:(NSString *)newValue;
+- (void)change_fontStyle:(NSString *)newValue;
+- (void)change_hint:(NSString *)newValue;
+- (void)change_inputType:(NSString *)newValue;
+- (void)change_password:(NSString *)newValue;
+- (void)change_clearAll:(NSString *)newValue;
+
+@end
