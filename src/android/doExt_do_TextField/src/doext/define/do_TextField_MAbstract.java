757b5dd545c607ee080cb0fbb2f47f9d5c3b3bd6
maoruiily <maoruiily@163.com>
2015-04-16 21:18:56 +0800

    update

---
+++ src/android/doExt_do_TextField/src/doext/define/do_TextField_MAbstract.java
@@ -0,0 +1,30 @@
+package doext.define;
+
+import core.object.DoUIModule;
+import core.object.DoProperty;
+import core.object.DoProperty.PropertyDataType;
+
+
+public abstract class do_TextField_MAbstract extends DoUIModule{
+
+	protected do_TextField_MAbstract() throws Exception {
+		super();
+	}
+	
+	/**
+	 * 初始化
+	 */
+	@Override
+	public void onInit() throws Exception{
+        super.onInit();
+        //注册属性
+		this.registProperty(new DoProperty("text", PropertyDataType.String, "", false));
+		this.registProperty(new DoProperty("fontColor", PropertyDataType.String, "000000", false));
+		this.registProperty(new DoProperty("fontSize", PropertyDataType.Number, "9", false));
+		this.registProperty(new DoProperty("fontStyle", PropertyDataType.String, "normal", false));
+		this.registProperty(new DoProperty("hint", PropertyDataType.String, "", true));
+		this.registProperty(new DoProperty("inputType", PropertyDataType.String, "", true));
+		this.registProperty(new DoProperty("password", PropertyDataType.Bool, "false", true));
+		this.registProperty(new DoProperty("clearAll", PropertyDataType.Bool, "false", true));
+	}
+}
